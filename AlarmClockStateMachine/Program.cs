93a52ccbbd329ebee5d3e5ffd267b8d3abc1153d
lkukhar <16503599+lkukhar@users.noreply.github.com>
2019-01-15 09:15:59 -0500

    Add project files.

---
+++ AlarmClockStateMachine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlarmClockStateMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
